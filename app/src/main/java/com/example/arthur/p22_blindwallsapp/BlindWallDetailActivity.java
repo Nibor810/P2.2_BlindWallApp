245da2a0ab9e2b5ee498d10eddb9d6b6cbd40cf7
Unknown <rm.hobbel@telfort.nl>
2017-11-09 15:48:51 +0100

    Added Detail Activity

---
+++ app/src/main/java/com/example/arthur/p22_blindwallsapp/BlindWallDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.arthur.p22_blindwallsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlindWallDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blind_wall_detail);
+    }
+}
